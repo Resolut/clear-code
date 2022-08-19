6093c3e1d2e0f0fd9793f5ccda9482faf1dc3928
Dmitriy <samdimus@gmail.com>
2022-08-19 22:06:21 +0300

    add homework file with variables naming for lesson_1

---
+++ lesson_01.py
@@ -0,0 +1,40 @@
+# Ясный код. Урок 1
+
+# Примеры плохих и хороших имен для переменных
+# было -> стало
+
+# 1 o_state -> order_current_status
+# текущий статус заявки
+
+# 2 f_ext_lst -> target_extensions_of_file
+# динамически создаваемый список расширений файла
+
+# 3 r -> shell_run_command_out
+# вывод команды, исполненной в командной оболочке
+
+# 4 page -> person_search_page
+# объект html страницы поиска пользователя
+
+# 5 percent > percent_of_progress
+# процент прогресса выполнения запущенной операции
+
+# 6 max_length > max_length_of_password_policy
+# Максимальная длина пароля, определенная политикой паролей
+
+# 7 not_in_list -> closed_ports
+# список закрытых портов
+
+# 8 sleep_period -> child_thread_sleep_period_in_sec
+# время простоя в секундах дочернего потока
+
+# 9 upgrade -> apt_upgrade_shell_command
+# команда обновления пакетов для менеджера пакетов apt
+
+# 10 parsed -> parsed_cef_keys
+# словарь с разобранными ключами в формате CEF
+
+# 11 outdated -> audio_track_is_outdated
+# флаг, хранящий состояние обновления версии аудио-дорожки
+
+# 12 answer -> answer_for_client_text_template
+# шаблон сообщения-ответа бота клиенту
