85f7988270d97108279e4a6cbdb08108b6cecb06
Dmitriy <samdimus@gmail.com>
2022-08-27 22:49:05 +0300

    add first part of lesson

---
+++ cc_lesson_04.py
@@ -0,0 +1,8 @@
+# Ясный код. Урок 3. Именование булевых переменных
+
+# 7.1
+# 7.1.1 is_changeable -> allow_changes
+# 7.1.2 is_exists -> exists
+# 7.1.3 is_resizable -> can_resize
+# 7.1.4 not_checked_by_filter -> checked_by_filter
+# 7.1.5 has_no_bill_data -> has_bill_data
