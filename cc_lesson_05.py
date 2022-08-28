d7501ccbf779c19919edc3cb6373fbb71fc6a70f
Dmitriy <samdimus@gmail.com>
2022-08-28 21:02:23 +0300

    add answers for exercises

---
+++ cc_lesson_05.py
@@ -0,0 +1,37 @@
+# Ясный код. Урок 5
+
+# 1 products -> product_count
+# переменная счетчика кол-ва продуктов,хранящая целое число, а не список продуктов
+
+# 2 c_cat -> client_category
+# категория клиента
+
+# 3 toggle -> toggle_country_selector
+# элемент селектор для выбора страны
+
+# 4 session -> open_session
+# объект текущей открытой сессии
+
+# 5 event -> scp_event
+# событие обмена по протоколу SCP
+
+# 6 has_promotion -> has_discount
+# булевая переменная, хранящая состояние о налиичии клиента скидки
+
+# 7 offset -> track_offset_ms
+# смещение аудио дорожки в миллисекундах
+
+# 8 input -> user_raw_input_stream
+# переменная потока ввода необработанных (сырых) данных от пользователя
+
+# 9 des_host -> target_hostname
+# имя целевого хоста
+
+# 10 log_user -> logged_in_user
+# объект залогиненного в систему пользователя
+
+# 11 DisconnectionException -> SessionConnectionError
+# класс-исключение соединения сессии
+
+# 12 win_module - win_platform_specific_module
+# название модуля для конкретной платформы
