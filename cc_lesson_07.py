7c991020f57d563e2c5590eb102ff3ed72266bdc
Dmitriy <samdimus@gmail.com>
2022-08-30 01:26:47 +0300

    add 2 answers for exercise

---
+++ cc_lesson_07.py
@@ -0,0 +1,30 @@
+# Ясный код. Урок 7
+
+# 5.1 incidents_count -> get_total_incidents_count
+# метод возвращает общее кол-во инцидентов в системе
+
+# 5.2  метод ничего не индексирует, а только изменяет формат даты в списке поля frames
+#      поэтому следует переименовать его в index -> reformat_time_for_each_frame()
+#      def index(self, time_format="%Y-%m-%d %H:%M:%S"):
+#          return [a.strftime(time_format) for a in self.frames]
+
+# 5.3
+
+# 5.4
+
+# 5.5
+
+# 5.6
+
+# 5.7
+
+# 5.8
+
+# 5.9
+
+# 5.10
+
+# 5.11
+
+# 5.12
+
